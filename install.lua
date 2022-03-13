c4a3cc5d1e93225091d452be44697bd63b51ceb1
TylenolWasTaken <tylenolwastaken@protonmail.com>
2022-03-13 16:00:39 -0400

    install.lua

---
+++ install.lua
@@ -0,0 +1,8 @@
+mkdir jaestd
+
+wget https://raw.githubusercontent.com/TylenolWasTaken/jaestd/master/jaestd/networking.lua jaestd/networking.lua
+wget https://raw.githubusercontent.com/TylenolWasTaken/jaestd/master/jaestd/mobility.lua jaestd/mobility.lua
+wget https://raw.githubusercontent.com/TylenolWasTaken/jaestd/master/jaestd/console.lua jaestd/console.lua
+
+print("Install finished")
+rm install.lua
