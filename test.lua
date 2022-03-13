3db2805bb6a75cbf159ef89e9b129ce1c2fcedc4
TylenolWasTaken <tylenolwastaken@protonmail.com>
2022-03-13 16:43:35 -0400

    test 1

--- test.lua
+++ test.lua
@@ -1,5 +1,12 @@
 function download_get(filename, url)
     local file = http.get(url)
+    if file[0] == nil then
+        for i in file do
+            print(i)
+        end
+        error("file url issue ig kys faggot", 0)
+    end
+
     open(filename)
     write(file)
     close(filename)
@@ -7,7 +14,7 @@ end
 
 function download_run(filename, url)
     download_get(filename, url)
-    if lines(filename)[0] == '--success'
+    if lines(filename)[0] == '--success' then
         os.run(filename)
     else
         error("error, corrupted or tampered file", 403)
