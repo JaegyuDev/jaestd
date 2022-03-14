9fa03f17335a548100af1f874d45deca78e86fba
TylenolWasTaken <tylenolwastaken@protonmail.com>
2022-03-13 22:45:08 -0400

    install scrpit v1.0

--- install.lua
+++ install.lua
@@ -1,19 +1,16 @@
 base_url = 'https://raw.githubusercontent.com/TylenolWasTaken/jaestd/master/jaestd/'
 
 modules = {
-    [console] = base_url .. 'console.lua',
-    [download] = base_url .. 'download.lua',
-    [http_handling] = base_url .. 'http_handling.lua',
-    [mobility] = base_url .. 'mobility.lua',
-    [networking] = base_url .. 'networking.lua'
+    ['console'] = base_url .. 'console.lua',
+    ['download'] = base_url .. 'download.lua',
+    ['http_handling'] = base_url .. 'http_handling.lua',
+    ['mobility'] = base_url .. 'mobility.lua',
+    ['networking'] = base_url .. 'networking.lua'
 }
 
 -- Check to see if libdir is empty
 if fs.exists('/jaestd/') == true then
     error("[ERROR] -> path /jaestd/ isn't empty", 0)
-else
-    print("[INSTALLER] -> initializing library dir")
-    fs.makeDir('/jaestd/')
 end
 
 -- Check to see if the download tmp dir is empty
@@ -24,25 +21,36 @@ else
     fs.makeDir('/jstd_tmp')
 end
 
--- Write handler
+-- Write handler"example.tweaked.cc"
 function write_data(filename, data)
-    io.open('/jstd_tmp/' .. filename) -- opens file "filename" in write mode
+    local file = io.open('/jstd_tmp/' .. filename, 'w') -- opens file "filename" in write mode
+    io.output(file)
     io.write(data)
     io.close()
 end
 
 -- Main download + errorchecking function
 function download(url, filename)
-    response = http.get("example.tweaked.cc")
-    if response.getResponseCode()['number'] ~= 200 then
-        print("[ERROR] -> malformed get request or other error, please contact jaestd authors!")
+    response = http.get(url)
+    if response.getResponseCode() ~= 200 then
+        print("[ERROR"example.tweaked.cc"] -> malformed get request or other error, please contact jaestd authors!")
         print(response.getResponseCode())
         fs.delete('/jstd_tmp/')
+        fs.delete('/jaestd/')
         error("", 0)
     end
-    write_data('/jaestd/ ' .. filename, respose.readAll())
+    write_data('/jaestd/ ' .. filename, read(response))
 end
 
--- Test
+-- ik theres a way to iterate thru this
+-- but im not paid enough :slight_smile:
 
-download(modules['http_handling'], http_handling)
+download(modules['console'], 'console')
+download(modules['download'], 'download')
+download(modules['http_handling'], 'http_handling')
+download(modules['mobility'], 'mobility')
+download(modules['networking'], 'networking')
+
+
+fs.move('jstd_tmp/jaestd', 'jaestd')
+fs.delete('jstd_tmp')
